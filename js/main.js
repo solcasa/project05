f5564f11ce83d8d333968b671f1aded47cacf43e
Gyuri Kim <133613660+solcasa@users.noreply.github.com>
2023-08-18 18:17:24 +0900

    230818
    230818

--- js/main.js
+++ js/main.js
@@ -5,10 +5,10 @@ $(function () {
         sct > 0 ? $('.header').addClass('on') : $('.header').removeClass('on');
     });
 
+
     $('.main_slide').on('init afterChange', function (e, s, c) {
         const current = $('.main_slide .slick-current');
         current.addClass('on').siblings().removeClass('on');
-
     });
 
     $('.main_slide').slick({
@@ -16,18 +16,28 @@ $(function () {
         autoplay: true,
         autoplaySpeed: 5000,
         fade: true,
+        pauseOnHover: false,
     });
 
     $('.main_visual .arrows .left').on('click', function () {
         $('.main_slide').slick('slickPrev')
     });
+
     $('.main_visual .arrows .right').on('click', function () {
         $('.main_slide').slick('slickNext')
     });
 
+
+    $('.scr').on('click', function (e) {
+        e.preventDefault();
+        const st = $(this.hash).offset().top;
+        console.log(st);
+        $('html, body').animate({ scrollTop: st }, 600)
+    });
+
+
     $('.portfolio_slide').slick({
         centerMode: true,
-
         variableWidth: true,
         arrows: false,
         dots: true,
@@ -36,16 +46,16 @@ $(function () {
     $('.main_portfolio .tab_arrows .left').on('click', function () {
         $('.portfolio_slide').slick('slickPrev')
     });
+
     $('.main_portfolio .tab_arrows .right').on('click', function () {
         $('.portfolio_slide').slick('slickNext')
     });
 
-    $('.scr').on('click', function (e) {
-        e.preventDefault();
-        const st = $(this.hash).offset().top;
-        console.log(st);
-        $('html, body').animate({ scrollTop: st }, 600)
-    });
+
+
+
+
+
 
     $(function () {
         $('.to_top').on('click', function () {
